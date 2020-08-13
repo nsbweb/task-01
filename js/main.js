23e88eede0eb05db132bf655c302a78b0d412b53
nsbweb <narendrabait@gmail.com>
2020-08-12 20:13:58 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+$('#wave').wavify({
+    height: 4, // Height of wave
+    amplitude: 30, // Amplitude of wave        
+    speed: .15, // Animation speed         
+    bones: 10, // Total number of articulation in wave
+    color: 'rgba(152,56,226, 0.90)' // Color
+});
+$('#wave-2').wavify({
+    height: 2, // Height of wave
+    amplitude: 30, // Amplitude of wave        
+    speed: .10, // Animation speed         
+    bones: 10, // Total number of articulation in wave
+    color: 'rgba(73,43,225, 0.90)' // Color
+});
+$('#wave-3').wavify({
+    height: 2, // Height of wave
+    amplitude: 30, // Amplitude of wave        
+    speed: .10, // Animation speed         
+    bones: 3, // Total number of articulation in wave
+    color: 'rgba(45,125,224, 0.90)' // Color
+});
+$('#wave-4').wavify({
+    height: 1, // Height of wave
+    amplitude: 30, // Amplitude of wave        
+    speed: .10, // Animation speed         
+    bones: 15, // Total number of articulation in wave
+    color: 'rgba(51,199,225, 0.90)' // Color
+});
+$('#flask-1').wavify({
+    height: 20, // Height of wave
+    amplitude: 50, // Amplitude of wave        
+    speed: .10, // Animation speed         
+    bones: 10, // Total number of articulation in wave
+    color: 'rgba(41,237,151, 1)' // Color
+});
+$('#flask-2').wavify({
+    height: 20, // Height of wave
+    amplitude: 40, // Amplitude of wave        
+    speed: .10, // Animation speed         
+    bones: 8, // Total number of articulation in wave
+    color: 'rgba(41,237,151, 1)' // Color
+});
