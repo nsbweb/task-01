b066c74fee52b7838e664b4bc9dc7c70587ecc5c
nsbweb <narendrabait@gmail.com>
2020-08-13 22:06:33 -0700

    Third Update

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
 
 $('#wave').wavify({
     height: 4, // Height of wave
-    amplitude: 30, // Amplitude of wave        
+    amplitude: 8, // Amplitude of wave        
     speed: .15, // Animation speed         
     bones: 10, // Total number of articulation in wave
-    color: 'rgba(152,56,226, 0.90)' // Color
+    color: 'rgba(152,56,226, 0.30)' // Color
 });
 $('#wave-2').wavify({
     height: 2, // Height of wave
-    amplitude: 30, // Amplitude of wave        
+    amplitude: 4, // Amplitude of wave        
     speed: .10, // Animation speed         
-    bones: 10, // Total number of articulation in wave
-    color: 'rgba(73,43,225, 0.90)' // Color
+    bones: 30, // Total number of articulation in wave
+    color: 'rgba(73,43,225, 0.30)' // Color
 });
 $('#wave-3').wavify({
     height: 2, // Height of wave
-    amplitude: 30, // Amplitude of wave        
+    amplitude: 1, // Amplitude of wave        
     speed: .10, // Animation speed         
-    bones: 3, // Total number of articulation in wave
+    bones: 35, // Total number of articulation in wave
     color: 'rgba(45,125,224, 0.90)' // Color
 });
 $('#wave-4').wavify({
-    height: 1, // Height of wave
-    amplitude: 30, // Amplitude of wave        
-    speed: .10, // Animation speed         
-    bones: 15, // Total number of articulation in wave
+    height: 0.5, // Height of wave
+    amplitude: 5, // Amplitude of wave        
+    speed: .20, // Animation speed         
+    bones: 30, // Total number of articulation in wave
     color: 'rgba(51,199,225, 0.90)' // Color
 });
 $('#flask-1').wavify({
-    height: 20, // Height of wave
-    amplitude: 50, // Amplitude of wave        
+    height: 30, // Height of wave
+    amplitude: 20, // Amplitude of wave        
     speed: .10, // Animation speed         
     bones: 10, // Total number of articulation in wave
     color: 'rgba(41,237,151, 1)' // Color
 });
 $('#flask-2').wavify({
-    height: 20, // Height of wave
-    amplitude: 40, // Amplitude of wave        
+    height: 30, // Height of wave
+    amplitude: 25, // Amplitude of wave        
     speed: .10, // Animation speed         
-    bones: 8, // Total number of articulation in wave
+    bones: 18, // Total number of articulation in wave
     color: 'rgba(41,237,151, 1)' // Color
 });
